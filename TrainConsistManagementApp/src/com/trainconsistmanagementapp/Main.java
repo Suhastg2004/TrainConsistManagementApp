72b9dd1f2db9113d2de20a58d8d444ae68497a3d
Suhastg2004 <suhastg263399@gmail.com>
2026-03-12 12:13:13 +0530

    Coded UC12 for safety check

--- TrainConsistManagementApp/src/com/trainconsistmanagementapp/Main.java
+++ TrainConsistManagementApp/src/com/trainconsistmanagementapp/Main.java
@@ -21,6 +21,7 @@ public class Main {
             this.type = type;
             this.cargo = cargo;
         }
+        
 
         public String getType()  { return type; }
         public String getCargo() { return cargo; }
